3a0501d3c6fc0cfb7534a4b68cbb90e8bb4c51f4
Vishal Singla <s.i.n.g.l.a.v.i.s.h.a.l.s.v@gmail.com>
2022-09-28 13:54:59 +0530

    Fethed one data

--- data.py
+++ data.py
@@ -3,6 +3,6 @@ import mysql.connector
 mydb = mysql.connector.connect(host="localhost", user="root", passwd="123456", database="emp")
 mycursor = mydb.cursor()
 mycursor.execute("select * from emp_system")
-
-for i in mycursor:
+result=mycursor.fetchone()
+for i in result:
     print(i)
